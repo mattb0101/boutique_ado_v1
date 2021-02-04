6a5056b033729ad7422289729e050e71ff074012
mattb0101 <mattbaum666@googlemail.com>
2021-02-04 16:55:27 +0000

    updated alluth templates and tested profiles

--- profiles/models.py
+++ profiles/models.py
@@ -8,8 +8,8 @@ from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
     """
-    A User profile model for maintaining default
-    deliveryu information and order history
+    A user profile model for maintaining default
+    delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
@@ -27,7 +27,7 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-    Create ofr update the user profile
+    Create or update the user profile
     """
     if created:
         UserProfile.objects.create(user=instance)
